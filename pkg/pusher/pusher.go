9985370c58c0782aae360fd13ee54d6241f53040
Ori Shussman <orishuss@gmail.com>
2025-04-08 12:44:53 +0000

    pushed set file

--- pkg/pusher/pusher.go
+++ pkg/pusher/pusher.go
@@ -24,7 +24,7 @@ func (p *Pusher) AddSet(set *set.Set, opts PushedSetOptions) {
 
 	pushedSet := newPushedSet(p.ctx, set, opts, p.opts)
 	p.sets = append(p.sets, pushedSet)
-	pushedSet.startLoop()
+	go pushedSet.startLoop()
 }
 
 func (p *Pusher) RemoveSet(set *set.Set) {
